293c2036fbf648deb8a86c04cf35df3ba21a7a0b
hkctkuy <hkctkuy@gmail.com>
2023-03-25 21:34:15 +0300

    Update Exercise

--- src/main/java/labs/webtech/table/Exercise.java
+++ src/main/java/labs/webtech/table/Exercise.java
@@ -21,19 +21,9 @@ public class Exercise implements TableEntity<Long>{
     private Long id;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "group_id")
-    @NonNull
-    private Group group;
-
-    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "course_id")
     @NonNull
     private Course course;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "lecturer_id")
-    @NonNull
-    private Lecturer lecturer;
 
     @Override
     public boolean equals(Object _other) {
@@ -41,8 +31,6 @@ public class Exercise implements TableEntity<Long>{
         if (_other == null || getClass() != _other.getClass()) return false;
         Exercise other = (Exercise) _other;
         return Objects.equals(id, other.id)
-                && Objects.equals(group, other.group)
-                && Objects.equals(course, other.course)
-                && Objects.equals(lecturer, other.lecturer);
+                && Objects.equals(course, other.course);
     }
 }
